abfa436270ce5c1237a4063f9646c1992ee6ba32
Rakshitha <rakshishetty142@gmail.com>
2025-05-07 10:36:55 +0530

    rakshishetty142@gmail.com

---
+++ src/main/java/project10/project10/App.java
@@ -0,0 +1,13 @@
+package project10.project10;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
